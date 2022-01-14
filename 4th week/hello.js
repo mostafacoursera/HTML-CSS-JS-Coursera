2ba2acfe0b343c83b3686061beae428b431a77de
mostafacoursera <96475038+mostafacoursera@users.noreply.github.com>
2022-01-14 21:24:32 +0200

    Update and rename hello.js to Hello.js

--- 4th week/hello.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-var Hello = {};
-var Message_Hello = "Hello";
-Hello.speak = function (name) {
-  console.log(Message_Hello + " " + name);
-}
-window.Hello = Hello;
-})(window);
